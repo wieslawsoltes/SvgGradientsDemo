60dacf2de3de91a34351f5898396f6b2762cb526
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-08-05 07:17:25 +0200

    Add SandBox

---
+++ SvgGradientsDemo/Pages/SandBox.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SvgGradientsDemo.Pages
+{
+    /// <summary>
+    /// Interaction logic for SandBox.xaml
+    /// </summary>
+    public partial class SandBox : UserControl
+    {
+        public SandBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
